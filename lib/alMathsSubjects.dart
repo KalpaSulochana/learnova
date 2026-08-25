1a393df115ee18af10a0a1f6ae6de7066b20f37e
unknown <kalpasulochana99@gmail.com>
2026-08-25 15:06:54 +0530

    create the resource screen, now it is available to all subjects

--- lib/alMathsSubjects.dart
+++ lib/alMathsSubjects.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:learnova/card.dart';
+import 'package:learnova/colors.dart';
+import 'package:learnova/resources/Advanced%20Level/Physical%20Science/CombinedMaths.dart';
 
 class AlmathssubjectScreen extends StatelessWidget {
   const AlmathssubjectScreen({super.key});
@@ -7,15 +10,29 @@ class AlmathssubjectScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+    
       home: Scaffold(
-        body: Column(
+        appBar: AppBar(
+        backgroundColor: bgColor,
+        elevation: 100,
+        title: Text(
+          "Select subject",
+          style: TextStyle(color: primaryBlue, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+        body: SingleChildScrollView(
+          child:Column(
           children: [
-            Center(
-              child: Text("hi"),
-            )
+            SizedBox(height: 10,),
+            buildCommonCard(context, Icons.functions, "Combined Maths", "Past Papers, Marking Schemes", combinedMaths()),
+            buildCommonCard(context, Icons.functions, "Physics", "Past Papers, Marking Schemes", combinedMaths()),
+            buildCommonCard(context, Icons.functions, "Chemistry", "Past Papers, Marking Schemes", combinedMaths()),
+            buildCommonCard(context, Icons.functions, "ICT", "Past Papers, Marking Schemes", combinedMaths()),
           ],
         ),
       ),
+    ),
     );
   }
 }
